53facc3a554fb346bd3918bc281e26a474c0ea92
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2023-01-03 11:57:24 +0100

    Initial commit

---
+++ schemas/externalresource.js
@@ -0,0 +1,71 @@
+export default {
+  name: 'externalresource',
+  title: 'External resource',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'tag',
+      title: 'Tag',
+      type: 'string',
+    },
+    {
+      name: 'featured',
+      title: 'Featured',
+      type: 'string',
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      })
+    },
+  },
+}
